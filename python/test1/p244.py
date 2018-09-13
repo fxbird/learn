13a9f3a42e72690cd16205e128efa8f4192bc560
Kurt <dldgxu@cn.ibm.com>
2018-09-13 23:14:21 +0800

    finish 300 pages

---
+++ python/test1/p244.py
@@ -0,0 +1,15 @@
+import socket
+
+s=socket.socket()
+
+host=socket.gethostname()
+port=1234
+s.bind((host,port))
+
+print('host : ',host)
+s.listen(5)
+while True:
+    c,addr=s.accept()
+    print('Got connection from ',addr)
+    c.send('Thank you for connectiong')
+    c.close
