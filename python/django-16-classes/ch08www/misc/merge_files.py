689d41204ae48ab36a1a338adf7a2b4459839c29
Kurt <dldgxu@cn.ibm.com>
2018-12-08 20:09:03 +0800

    delete venv2

---
+++ python/django-16-classes/ch08www/misc/merge_files.py
@@ -0,0 +1,21 @@
+import os
+from docx import Document
+
+path=input('请输入完整的目录:\n').rstrip(r'\/')
+# path=r'M:\My-Documents\programming\java\source\Python Django Practise\django1\course'
+result=path+'\n'
+for fname in os.listdir(path):
+   fullPath=os.path.join(path,fname)
+   if os.path.isfile(fullPath) and not fullPath.find('init')>-1:
+       result+=fname.center(80,'*')+'\n'
+       f=open(fullPath,'r',encoding='utf-8')
+       result+=f.read()
+       result+='\n\n\n'
+       f.close()
+
+print(result)
+folder_name=os.path.join('source',path.split('\\')[-1])
+print('Folder name :',folder_name)
+document =Document()
+document.add_paragraph(result)
+document.save(folder_name+'.docx')
