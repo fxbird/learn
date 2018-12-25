f09b1ce7c9b62c64734ced0f84724ff35c7eca69
Kurt <dldgxu@cn.ibm.com>
2018-12-25 22:13:07 +0800

    8d , unfinish

--- python/flask/app/__init__.py
+++ python/flask/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from config import config
@@ -9,6 +10,10 @@ bootstrap=Bootstrap()
 mail=Mail()
 moment=Moment()
 db=SQLAlchemy()
+
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 
 def create_app(config_name):
     app=Flask(__name__)
@@ -23,4 +28,5 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_bluep
     return app
